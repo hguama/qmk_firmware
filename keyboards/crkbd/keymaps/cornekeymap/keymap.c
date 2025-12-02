2528c7bf051f2146fe5479a51a37ce3ddce073cb
Hero <guamaoh@gmail.com>
2025-12-01 21:23:28 -0500

    update

--- keyboards/crkbd/keymaps/cornekeymap/keymap.c
+++ keyboards/crkbd/keymaps/cornekeymap/keymap.c
@@ -1741,14 +1741,14 @@ tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-y     //_BASE Layer LT(ALT_SHIFT,_MOUSE_1)
+    //_BASE Layer LT(ALT_SHIFT,_MOUSE_1)
     [_BASE] = LAYOUT_split_3x6_3(
 // ,-------------------------------------------------------------------------------------.                ,-----------------------------------------------------.
 LCTL_T(KC_F3), LT(_SYMB,KC_ESC), ALT_TAB, LT(_NUMB,KC_TAB), C(KC_X), QK_BOOT,                              QK_BOOT, XXXXXXX, TD(TDQ_SEL), LT(_RUN,KC_HOME), LT(_SYMB,KC_END), XXXXXXX,
 // |--------+--------+--------+--------+--------+----------------------------------------|                |--------+--------+--------+--------+--------+--------|
 LT(_AI,_MOUSE_2), LT(_DEL,TG_0), LT(_MOVE_H, COPY), LT(_MOVE_V, PASTE), LT(SEL_ALL,KC_SPACE), C(KC_S),     SLEEP, LT(0,SEL_W_ALL), LT(_BOOK,KC_LEFT), LT(_MOVE_WIN,KC_DOWN), LT(_MOVE_L,KC_RIGHT), LT(_AI,KC_UP),
 // |--------+--------+--------+--------+--------+----------------------------------------|                |--------+--------+--------+--------+--------+--------|
-batman A(KC_F4), LT(_AI,SHIFT_TOGGLE), G(KC_T), LT(_BOOK,CTRL_Z), XXXXXXX, WIN_D,                   HIBERNATE, XXXXXXX, TG(_MODE), LT(0,SHOW_QUICK_ENT), LT(0,CODE_COMPLET), KC_INS,
+A(KC_F4), LT(_AI,SHIFT_TOGGLE), G(KC_T), LT(_BOOK,CTRL_Z), XXXXXXX, WIN_D,                                 HIBERNATE, XXXXXXX, TG(_MODE), LT(0,SHOW_QUICK_ENT), LT(0,CODE_COMPLET), KC_INS,
 // |--------+--------+--------+--------+--------+--------+-------------------------------|                |--------+--------+--------+--------+--------+--------+--------|
                                     				   LT(_DEV,KC_ENT), KC_LGUI, KC_LALT,                TO(_BASE), XXXXXXX, LT(_DEV,KC_SPACE)
                                                        // `----------------------------------'              `---------------------------------'
@@ -1970,9 +1970,9 @@ batman A(KC_F4), LT(_AI,SHIFT_TOGGLE), G(KC_T), LT(_BOOK,CTRL_Z), XXXXXXX, WIN_D
     // |--------+--------+--------+--------+--------+--------|                                             |--------+--------+--------+--------+--------+--------|
     LT(KC_MS_ACCEL2, TG_0), KC_BTN1, KC_MS_D, KC_MS_U, LT(SEL_ALL,KC_SPACE), XXXXXXX,                       SLEEP, LT(KC_MS_WH_LEFT,KC_MS_WH_RIGHT), KC_MS_L, KC_MS_R, KC_MS_WH_DOWN, KC_MS_WH_UP,
     // |--------+--------+--------+--------+--------+--------|                                             |--------+--------+--------+--------+--------+--------|
-    A(KC_F4), LT(PASTE,COPY), G(KC_T), C(KC_Z), XXXXXXX, WIN_D,                                            XXXXXXX, HIBERNATE, KC_BTN2, LT(0,PGDN_PGUP), XXXXXXX, XXXXXXX,
+    A(KC_F4), LT(PASTE,COPY), G(KC_T), C(KC_Z), XXXXXXX, WIN_D,                                            HIBERNATE, XXXXXXX, KC_BTN2, LT(0,PGDN_PGUP), XXXXXXX, XXXXXXX,
     // |--------+--------+--------+--------+--------+--------|                                             |--------+--------+--------+--------+--------+--------+--------|
-                                       KC_ENT,LT(KC_MS_WH_LEFT,KC_MS_WH_RIGHT), KC_LALT,                      TO(_BASE), XXXXXXX, KC_SPC
+                                       KC_ENT,LT(KC_MS_WH_LEFT,KC_MS_WH_RIGHT), KC_LCTL,                      TO(_BASE), XXXXXXX, KC_SPC
                                        // `---------------------'                                           `--------------------------'
     ),
 
