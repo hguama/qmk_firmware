407c68f24c5de1c6302305cc38a3ae9013a37583
Hero <guamaoh@gmail.com>
2026-05-19 11:32:04 -0500

    update to RGB MATRIX

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -3,7 +3,7 @@ MOUSEKEY_ENABLE     = yes
 RAW_ENABLE          = yes
 RGBLIGHT_ENABLE     = no  # Activa la iluminación RGB
 RGB_MATRIX_ENABLE   = yes   # Activa RGB matrix (RGBLIGHT_ENABLE debe estar en NO)
-COMBO_ENABLE        = no
+COMBO_ENABLE        = yes
 REPEAT_KEY_ENABLE   = no
 
 #for reduce size firmware
